ce3fe9d70279e0257deca69e246b394ce7a69695
umar2code <m.jumar@hotmail.com>
2021-06-09 22:41:44 +0400

    added changes for firebase

--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from "redux";
-import {USERS_FEATURE_KEY, usersReducer} from "./users/users.reducer";
-
-let rootReducer = combineReducers({
-    [USERS_FEATURE_KEY] : usersReducer,
-});
-
-export {rootReducer};
