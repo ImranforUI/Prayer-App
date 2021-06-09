c1d4fbb953835f025398d788b99919504feb8111
umar2code <m.jumar@hotmail.com>
2021-06-09 22:39:40 +0400

    added changes

---
+++ build/precache-manifest.2b1b649d3427416f1bdbaf19c7725e14.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161afa231fd15d19dd938d77176c312f",
+    "url": "/ImranforUI/First-App.git/index.html"
+  },
+  {
+    "revision": "aabfa1c9d1549f94f7d9",
+    "url": "/ImranforUI/First-App.git/static/css/2.bce18547.chunk.css"
+  },
+  {
+    "revision": "41607c6df897482a0300",
+    "url": "/ImranforUI/First-App.git/static/css/main.ff3beba4.chunk.css"
+  },
+  {
+    "revision": "aabfa1c9d1549f94f7d9",
+    "url": "/ImranforUI/First-App.git/static/js/2.3ce76855.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ImranforUI/First-App.git/static/js/2.3ce76855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41607c6df897482a0300",
+    "url": "/ImranforUI/First-App.git/static/js/main.c8d6d04c.chunk.js"
+  },
+  {
+    "revision": "cbe89c8ac0f6fdd21700",
+    "url": "/ImranforUI/First-App.git/static/js/runtime-main.6e7bbc95.js"
+  },
+  {
+    "revision": "270f72df47e036d9cfa11f91f0a94a60",
+    "url": "/ImranforUI/First-App.git/static/media/arrow_left.270f72df.svg"
+  },
+  {
+    "revision": "d9ccc77c8240ad0dc2724cf333c0479e",
+    "url": "/ImranforUI/First-App.git/static/media/arrow_right.d9ccc77c.svg"
+  },
+  {
+    "revision": "8eb5085cde87951c23f10d7df8dd8f34",
+    "url": "/ImranforUI/First-App.git/static/media/bg-image2.8eb5085c.jpg"
+  },
+  {
+    "revision": "a10c6586060b5b2addb3a5d5ee4e79d6",
+    "url": "/ImranforUI/First-App.git/static/media/islamic-prayer-dusk-sun.a10c6586.jpeg"
+  }
+]);
